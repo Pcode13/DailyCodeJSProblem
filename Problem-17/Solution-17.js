868f7ab0e0a2e74499df7b2266c57afc101648fc
Pragati Veer <92431808+Pcode13@users.noreply.github.com>
2025-04-07 18:03:37 +0530

    update code 17

--- Problem-17/Solution-17.js
+++ Problem-17/Solution-17.js
@@ -14,4 +14,17 @@ function singleNumber(nums) {
   console.log(singleNumber([2, 2, 1]))  // Output: 1, 5, 0, 3, 11
 //
 //
-//Time Complexity: O(n), where n is the length of the input array nums. This is because we are iterating through the array once to count the occurrences of each number and then iterating through the count object to find the number that appears only once.  
+//Time Complexity: O(n), where n is the length of the input array nums. 
+// This is because we are iterating through the array once to count the occurrences of
+//  each number and then iterating through the count object to find the number that appears only once.  
+
+function singleNumber(nums) {
+    for (let num of nums) {
+        console.log(nums.indexOf(num),nums.lastIndexOf(num))
+      if (nums.indexOf(num) === nums.lastIndexOf(num)) {
+        return num;
+      }
+    }
+  }
+  console.log(singleNumber([2, 1,2,3,5,1,5]));
+  
